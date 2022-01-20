244cb87c5b0a67c02e80814075e9916c8e256fd2
grant <grantmossdev@gmail.com>
2022-01-20 11:46:17 -0700

    more progress on tutorial

--- src/components/tutorialText.js
+++ src/components/tutorialText.js
@@ -46,5 +46,33 @@ export const tutorialText = {
       width: 469,
       height: 428,
     },
+    kpTooltip: {
+      paragraphs: [
+        "Kilometer Posts (KP) are represented on the map as transparent maroon circles overlayed on top of the centerline. Hover over a KP to view the KP number and the project spread that the KP belongs to.",
+        "You can click on a KP to isolate the communities specific to that project spread. Once a KP is clicked, the map will zoom to fit all the communities for that spread, and the circles for these communities will change color to light blue. At this point, you can click on the <i>Reset Map</i> button or click on another KP to continue using the app.",
+      ],
+      path: "/images/tutorial/kp-tooltip.png",
+      width: 259,
+      height: 194,
+    },
+    reserveTooltip: {
+      paragraphs: [
+        "You will need to zoom in on the map much closer to accurately interact with this layer.",
+        "When hovering over a First Nations Reserve, a tooltip will appear with some preliminary information. The <i>Total overlap</i> number indicates the the total length of regulated assets that pass through the reserve boundary (TMX and existing mainline). You can then click on the reserve to view more information. See additional instructions below.",
+      ],
+      path: "/images/tutorial/reserve-tooltip.png",
+      width: 403,
+      height: 302,
+    },
+    reservePopup: {
+      paragraphs: [
+        "Clicking on a First Nations Reserve opens a pop-up with a breakdown of the pipeline overlaps with the reserve boundary, as well as nearby incidents.",
+        "If there are incidents within the reserve boundaries, or within 15km of the reserve boundary, they will appear as red circles on the map. These incidents are specific to the reserve that was just clicked. You can hover over the incident circle to view additional information about the incident. The red circles will remain on the map until another reserve is clicked, or the <i>Reset Map</i> button is clicked.",
+        'The incident data is sourced from CER, and is available on <a href="https://open.canada.ca/data/en/dataset/fd17f08f-f14d-433f-91df-c90a34e1e9a6" target="_blank" rel="noopener noreferrer">Open Government</a>',
+      ],
+      path: "/images/tutorial/reserve-popup.png",
+      width: 448,
+      height: 336,
+    },
   },
 };
